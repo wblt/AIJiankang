3d39da37f5de545b6b439cd1efcdd7e8281a5f5e
wblt <940422068@qq.com>
2018-08-14 18:32:45 +0800

    d

---
+++ AIHealthyBlockChain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AIHealthyBlockChain
+//
+//  Created by 冷婷 on 2018/8/14.
+//  Copyright © 2018年 冷婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
